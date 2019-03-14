ca433cf0ecf0cf4c2e5dd330cef273c95a93d62a
Tyler Gerwig <eit_tcgerwig@eit-studweb-mac.exnet.iastate.edu>
2019-03-14 16:59:15 -0500

    Added command

---
+++ src/Commands/StaffProfileSyncCommands.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\staff_profile_sync\Commands;
+
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ */
+class StaffProfileSyncCommands extends DrushCommands {
+
+  /**
+   * Sync drupal entities
+   * @usage staff_profile_sync-commandName sync-profiles
+   *  Used to sync staff profile entities.
+   *
+   * @command staff_profile_sync:sync
+   * @aliases sync-profiles
+   */
+  public function syncProfiles() {
+    drush_print("Staff Profile Sync Started");
+    staff_profile_sync_updater();
+    drush_print("Staff Profile Sync Finished");
+  }
+}
