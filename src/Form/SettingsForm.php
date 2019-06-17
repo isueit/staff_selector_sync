2ec407dd5748ce039e90dcffb2b1943af08246ef
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2019-06-17 17:00:50 -0500

    Added more secure storage of credentials

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -4,6 +4,7 @@ namespace Drupal\staff_profile_sync\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\encrypt\Entity\EncryptionProfile;
 
 /*
  * Class SettingsForm
@@ -48,9 +49,53 @@ class SettingsForm extends ConfigFormBase {
           '#markup' => 'Last Updated on: ' . $config->get('staff_profile_sync_last'),
         ];
       }
-      $form['note'] = [
-        '#markup' => 'Updating the database login and address must be done in the settings.php'
-      ];
+      $form['username'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Username'),
+        '#description' => $this->t('The username used to connect to the staff database.'),
+        '#maxlength' => 64,
+        '#size' => 64,
+        '#default_value' => $config->get('db_username'),
+      );
+      $form['password'] = array(
+        '#type' => 'password',
+        '#title' => $this->t('Password'),
+        '#description' => $this->t('The password used to connect to the staff database. Note: This is not a secure password storage facility, use an account with the fewest permissions. This field will always show up blank even when a password is saved.'),
+        '#size' => 64,
+        '#default_value' => $this->t(""),
+      );
+      $form['server_url'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('URL'),
+        '#description' => $this->t('The url used to connect to the staff database.'),
+        '#maxlength' => 64,
+        '#size' => 64,
+        '#default_value' => $config->get('db_address'),
+      );
+      $form['database'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Database'),
+        '#description' => $this->t('The database containing the staff profiles.'),
+        '#maxlength' => 64,
+        '#size' => 64,
+        '#default_value' => $config->get('db_database'),
+      );
+
+      // List of encryption profiles for selector
+      $encrypt_ids = \Drupal::entityQuery('encryption_profile')->execute();
+      $encrypt_list = array();
+      foreach ($encrypt_ids as $eid) {
+        $encrypt_prof = \Drupal::entityTypeManager()->getStorage('encryption_profile')->load($eid);
+        $encrypt_list[$eid] = $encrypt_prof->label();
+      }
+
+      $form['encrypt_profile'] = array(
+        '#type' => 'select',
+        '#title' => $this->t("Encryption Profile"),
+        '#description' => $this->t('Encryption profile used for encryption.'),
+        '#options' => $encrypt_list,
+        '#default_value' => $config->get('sync_encrypt_profile'),
+      );
 
       return parent::buildForm($form, $form_state);
     }
@@ -60,6 +105,17 @@ class SettingsForm extends ConfigFormBase {
      */
     public function validateForm(array &$form, FormStateInterface $form_state) {
       parent::validateForm($form, $form_state);
+
+      $config = $this->config('staff_profile_sync.settings');
+      $saved_pwd = $config->get('db_password');
+      $new_pwd = $form_state->getValue('password');
+      $encrypt_profile = EncryptionProfile::load($form_state->getValue('encrypt_profile'));
+
+      if (empty($new_pwd)) {
+        $form_state->setValue('password', $saved_pwd);
+      } else {
+        $form_state->setValue('password', \Drupal::service('encryption')->encrypt($new_pwd, $encrypt_profile));
+      }
     }
 
     /**
@@ -76,5 +132,13 @@ class SettingsForm extends ConfigFormBase {
         $this->config('staff_profile_sync.settings')
           ->set('staff_profile_sync_last', 0);
       }
+      $this->config('staff_profile_sync.settings')
+        ->set('db_username', $form_state->getValue('username'))
+        ->set('db_password', $form_state->getValue('password'))
+        ->set('db_address', $form_state->getValue('server_url'))
+        ->set('db_database', $form_state->getValue('database'))
+        ->set('sync_encrypt_profile', $form_state->getValue('encrypt_profile'))
+        ->save();
+
   }
 }
